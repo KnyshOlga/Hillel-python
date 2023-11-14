620b4455eb5f223588c7f20e85749d696c19f2bf
Knysh_Olga <olgakn56@gmail.com>
2023-11-14 11:24:00 +0300

    hw 6 done

---
+++ 06.py
@@ -0,0 +1,5 @@
+my_list = [12, 4, -2, 36, 0, 1, 22]
+last_element = my_list.pop(6)  # Видаляємо останній елемент зі списку
+my_list.insert(0, last_element)  # Додаємо його на початок
+print(my_list)
+

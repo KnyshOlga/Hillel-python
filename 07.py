c8ece3f09b6a0f788f313685f3c4a37f6447363a
Knysh_Olga <olgakn56@gmail.com>
2023-11-15 12:12:11 +0300

    hw 7 done

--- 07.py
+++ 07.py
@@ -1,4 +1,21 @@
-lst = [4, 0, 5, 78, 3, 14, 0]
-lst.extend([0])  #Переносив "0", але не видаляв зі списку
-lst.remove(0)   #Тоді зробила оце, мабуть неправильно
+#Один варіант
+lst = [4, 0, 5, 0, 0, 14, 22]
+lst1 = []
+lst2 = []
+for i in lst:
+    if i == 0:
+        lst2.append(i)
+    else:
+        lst1.append(i)
+lst_result = lst1 + lst2
+print(lst_result)
+
+
+#Другий варіант
+lst = [4, 0, 5, 0, 0, 14, 22, -1]
+for i in lst:
+    if i == 0:
+        lst.remove(i)
+        lst.append(i)
 print(lst)
+

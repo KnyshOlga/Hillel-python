de6d3efc9cef176a409eb410d7cbbc564aca4acf
Knysh_Olga <olgakn56@gmail.com>
2023-11-16 12:37:07 +0300

    hw 4 done

---
+++ 04-test.py
@@ -0,0 +1,19 @@
+first_number = int(input("Введіть перше число: "))
+second_number = int(input("Введіть друге число: "))
+operator = input("Введіть оператор (+, -, *, /): ")
+
+
+if operator == '+':
+    result = first_number + second_number
+elif operator == '-':
+    result = first_number - second_number
+elif operator == '*':
+    result = first_number * second_number
+elif operator == '/':
+    if second_number != 0:
+        result = first_number / second_number
+    else:
+        result = "Помилка! Ділення на нуль неможливе."
+print(result)
+
+

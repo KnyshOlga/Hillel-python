6246cda832e4b7de947b8a5b4c1431f255894a27
Knysh_Olga <olgakn56@gmail.com>
2023-11-22 15:30:02 +0300

    hw 17 done

--- 17.py
+++ 17.py
@@ -1,25 +1,22 @@
-
 def correct_sentence(text):
+    first_letter = text[0]
 
-  first_letter = text[0]
+    if not first_letter.isupper():
+        text = first_letter.upper() + text[1:]
 
-  if not first_letter.isupper():
-    text = first_letter.upper() + text[1:]
+    last_letter = text[-1]
 
-  last_letter = text[-1]
+    if last_letter != '.':
+        text = text + '.'
 
-  if last_letter != '.':
-    text = text + '.'
-
-  return text
+    return text
 
 
-print(correct_sentence("greetings, friends") == "Greetings, friends.")
-print(correct_sentence("hello") == "Hello.")
-print(correct_sentence("Greetings. Friends") == "Greetings. Friends.")
-print(correct_sentence("Greetings, friends.") == "Greetings, friends.")
-print(correct_sentence("greetings, friends.") == "Greetings, friends.")
-
+# print(correct_sentence("greetings, friends") == "Greetings, friends.")
+# print(correct_sentence("hello") == "Hello.")
+# print(correct_sentence("Greetings. Friends") == "Greetings. Friends.")
+# print(correct_sentence("Greetings, friends.") == "Greetings, friends.")
+# print(correct_sentence("greetings, friends.") == "Greetings, friends.")
 
 print(correct_sentence("greetings, friends"))
 print(correct_sentence("hello"))

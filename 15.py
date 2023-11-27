59ef46e82c56c81062678b4dc7b5ec1e26df8a47
Knysh_Olga <olgakn56@gmail.com>
2023-11-27 17:06:29 +0300

    hw 15 done

---
+++ 15.py
@@ -0,0 +1,25 @@
+
+def convert_seconds(seconds):
+    if seconds < 0 or seconds >= 8640000:
+        raise ValueError
+
+    days = seconds // (24 * 60 * 60)
+    seconds %= (24 * 60 * 60)
+    hours = seconds // (60 * 60)
+    seconds %= (60 * 60)
+    minutes = seconds // 60
+    seconds %= 60
+
+    days_str = "дні" if days > 1 else "день"
+    hours_str = f"0{hours:02d}" if hours < 10 else f"{hours:02d}"
+    minutes_str = f"0{minutes:02d}" if minutes < 10 else f"{minutes:02d}"
+    seconds_str = f"0{seconds:02d}" if seconds < 10 else f"{seconds:02d}"
+
+    return f"{days} {days_str}, {hours_str}:{minutes_str}:{seconds_str}"
+
+
+try:
+    seconds = int(input("Введіть кількість секунд: "))
+    print(convert_seconds(seconds))
+except ValueError as e:
+    print(e)

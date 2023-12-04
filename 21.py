8f8b194879bb56e8ba4297503e132165944785ed
Knysh_Olga <olgakn56@gmail.com>
2023-12-04 11:13:28 +0300

    hw 21 done

---
+++ 21.py
@@ -0,0 +1,12 @@
+def popular_words(text, words):
+    words_in_text = text.lower().split()
+
+    word_count = {word: words_in_text.count(word) for word in words}
+
+    return word_count
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+print('OK')
+

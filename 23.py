92938b63dd4b63beb9aa8d01186d7a0cb9be0dd0
Knysh_Olga <olgakn56@gmail.com>
2023-12-08 16:04:50 +0300

    hw 6 done

---
+++ 23.py
@@ -0,0 +1,16 @@
+def some_gen(begin, end, func):
+    i = 0
+    while i < end:
+        yield func(begin)
+        begin = func(begin)
+        i += 1
+        yield begin
+
+    from inspect import isgenerator
+
+    gen = some_gen(2, 4, pow)
+    assert isgenerator(gen) == True, 'Test1'
+    assert list(gen) == [2, 4, 16, 256], 'Test2'
+
+
+print('OK')

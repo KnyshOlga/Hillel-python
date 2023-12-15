03092ca2a1f0827e3c7066587b21ef011424b324
Knysh_Olga <olgakn56@gmail.com>
2023-12-15 12:15:52 +0300

    hw 23 done

--- 23.py
+++ 23.py
@@ -1,16 +1,37 @@
-def some_gen(begin, end, func):
+
+from inspect import isgenerator
+
+
+def generate_sequence(begin, end, func):
+
     i = 0
     while i < end:
-        yield func(begin)
+        yield begin
         begin = func(begin)
         i += 1
-        yield begin
-
-    from inspect import isgenerator
-
-    gen = some_gen(2, 4, pow)
-    assert isgenerator(gen) == True, 'Test1'
-    assert list(gen) == [2, 4, 16, 256], 'Test2'
 
 
-print('OK')
+def pow(x):
+
+    return x ** 2
+
+
+def test_generate_sequence():
+    # Перевіряю, що створений об'єкт - це генератор
+    gen_test1 = generate_sequence(2, 4, pow)
+    assert isgenerator(gen_test1), 'Тест 1'
+
+    # Перевіряю відповідність списку
+    generated_list_test2 = list(generate_sequence(2, 4, pow))
+    assert generated_list_test2 == [2, 4, 16, 256], 'Тест 2'
+
+
+def main():
+    # Приклад
+    gen = generate_sequence(2, 4, pow)
+    print(list(gen))
+
+
+if __name__ == "__main__":
+    test_generate_sequence()
+    main()

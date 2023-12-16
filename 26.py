238325695c679941e82c73faba37bc3a4c57c103
Knysh_Olga <olgakn56@gmail.com>
2023-12-16 14:01:51 +0300

    hw 26 done

--- 26.py
+++ 26.py
@@ -1,9 +1,13 @@
 def is_even(number):
-    while number % 2 == 0:
-        number //= 2
-    return number == 1
+    if number & 1:
+        return False
+    else:
+        return True
 
 
-    assert is_even(2494563894038 ** 2) == True, 'Test1'
-    assert is_even(1056897 ** 2) == False, 'Test2'
-    assert is_even(24945638940387 ** 3) == False, 'Test3'
+assert is_even(2494563894038 ** 2) == True, 'Test1'
+assert is_even(1056897 ** 2) == False, 'Test2'
+assert is_even(24945638940387 ** 3) == False, 'Test3'
+print(is_even(2494563894038 ** 2))
+print(is_even(1056897 ** 2))
+print(is_even(24945638940387 ** 3))
